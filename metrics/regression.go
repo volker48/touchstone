575b8750db34e3f7f4d9687e54bcb5344f1b56ca
Reed Rosenberg <rrosenberg@red-spark.com>
2017-10-24 13:15:29 -0400

    Adds ExplainedVariance to regression metrics.

--- metrics/regression.go
+++ metrics/regression.go
@@ -6,10 +6,12 @@ import (
 )
 
 type Residuals struct {
+	Res    float64
 	SS_Res float64
 	Sum    float64
 	Count  int64
 	ys     []float64
+	yHats  []float64
 }
 
 func (ss *Residuals) RSquared() float64 {
@@ -26,20 +28,36 @@ func (ss *Residuals) MSE() float64 {
 	return ss.SS_Res / float64(ss.Count)
 }
 
+func (ss *Residuals) ExplainedVariance() float64 {
+	y_diff_avg := ss.Res / float64(ss.Count)
+	y_true_avg := ss.Sum / float64(ss.Count)
+	var output float64
+	for i, y := range ss.ys {
+		n_term := y - ss.yHats[i] - y_diff_avg
+		numerator := n_term * n_term
+		d_term := y - y_true_avg
+		denominator := d_term * d_term
+		output += 1 - (numerator / denominator)
+	}
+	return output / float64(ss.Count)
+}
+
 func (ss *Residuals) Update(yText, yHatText string) {
 	y, err := strconv.ParseFloat(yText, 64)
 	if err != nil {
-		log.Fatal("Error parsing float: ", err)
+		log.Fatal("Error parsing y value as float: ", err)
 	}
 
 	yHat, err := strconv.ParseFloat(yHatText, 64)
 	if err != nil {
-		log.Fatal("Error parsing float: ", err)
+		log.Fatal("Error parsing yhat value as float: ", err)
 	}
 
 	ss.Count++
 	ss.Sum += y
 	e := y - yHat
+	ss.Res += e
 	ss.SS_Res += e * e
 	ss.ys = append(ss.ys, y)
+	ss.yHats = append(ss.yHats, yHat)
 }
